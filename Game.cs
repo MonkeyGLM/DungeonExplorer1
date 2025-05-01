d0d8f38507ba8a30aadb45f50e92ddf9d9359f88
MonkeyGLM <crusaderglm@gmail.com>
2025-05-01 11:26:43 +0100

    1

--- Game.cs
+++ Game.cs
@@ -11,15 +11,56 @@ namespace DungeonExplorer
         public Game()
         {
             // Initialize the game with one room and one player
-
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            player = new Player(name, 100);
+            currentRoom = new Room("It is a cramped, musty room of moss-covered cobbled walls and a carpeted floor. There is a small potion on the floor.", "Small Potion");
+            
         }
         public void Start()
         {
-            // Change the playing logic into true and populate the while loop
-            bool playing = false;
+            // Changed the playing logic into true and populated the while loop
+            bool playing = true;
+
+            Console.WriteLine("Welcome", player);
+            Console.WriteLine("You enter the dungeon.");
+
             while (playing)
             {
                 // Code your playing logic here
+                Console.WriteLine(currentRoom.GetDescription());
+                Console.WriteLine("What will you do?");
+                Console.WriteLine("- 1. Pick up the item");
+                Console.WriteLine("- 2. Check status");
+                Console.WriteLine("- 3. Flee like a coward"); 
+
+                string input = Console.ReadLine();
+                if (input == "1")
+                {
+                    if (!string.IsNullOrEmpty(currentRoom.Item))
+                    {
+                        player.PickUpItem(currentRoom.Item);
+                        Console.WriteLine("You picked up a "+ currentRoom.Item);
+                        currentRoom.RemoveItem();
+                    }
+                    else
+                    {
+                        Console.WriteLine("There is nothing to pick up.");
+                    }
+                }
+
+                if (input == "2")
+                {
+                    Console.WriteLine($"Health: {player.Health}");
+                    Console.WriteLine("Inventory: " + player.InventoryContents());
+                }
+                
+                if (input == "3")
+                {
+                    Console.WriteLine("Thanks for playing!");
+                    playing = false;
+                    break;
+                }
             }
         }
     }
--- Room.cs
+++ Room.cs
@@ -3,15 +3,31 @@
     public class Room
     {
         private string description;
+        private string item;
 
-        public Room(string description)
+        public Room(string description, string item)
         {
             this.description = description;
+            this.item = item;
         }
 
         public string GetDescription()
         {
             return description;
+        }
+        public string Item 
+        {
+            get { return item; } 
+        }
+
+        public void PickedUpItem()
+        {
+            item = null;
+        }
+        
+        public void RemoveItem()
+        {
+            item = null;
         }
     }
 }
